70cc1b77234d5bf06e75a86908f8fa7cb1efef8b
Teller35 <tellerwetzel@yahoo.com>
2021-09-01 09:16:57 -0600

    downloaded dependices and set up App.js to do router, apollo client

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import "./App.css";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <div>
-      <header>
-        Reminders
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" />
+          </Switch>
+          <header>Reminders</header>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
