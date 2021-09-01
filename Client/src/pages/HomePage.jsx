f9f4cfd1bfadc3165b397f76b88937c5cbc134f5
Teller35 <tellerwetzel@yahoo.com>
2021-09-01 13:06:46 -0600

    have start to homepage pulls user and reminders with auth inforced

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,11 +1,57 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
+import { Link, useParams } from "react-router-dom";
+import Auth from "../utils/auth";
 
 const HomePage = () => {
+  const { username: userParam } = useParams();
+
+  const { data } = useQuery(GET_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || [];
+
+  const reminders = user.reminders || [];
+
+  const loggedIn = Auth.loggedIn();
+
+  if (!reminders.length) {
     return (
-        <section>
-            <h1>Welcome</h1>
-        </section>
-    )
-}
+      <section>
+        {loggedIn && (
+          <>
+            <h1>Welcome {user.username}</h1>
+            <h3>
+              Lets gather some info to create that reminder click{" "}
+              <Link to="/create">here</Link>
+            </h3>
+            <div>
+              <h3>You have no reminders....</h3>
+            </div>
+          </>
+        )}
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      {loggedIn && (
+        <>
+          <h1>Welcome {user.username}</h1>
+          <h3>
+            Lets gather some info to create that reminder click{" "}
+            <Link to="/create">here</Link>
+          </h3>
+          <div>
+            <h3>Here is what you have scheduled...</h3>
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
 
 export default HomePage;
